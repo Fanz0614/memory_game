3afb59820028dfca06110a9c5e1cab9c8c14b8d5
Jacky Zhang <jackyzh0614@gmail.com>
2023-04-16 12:38:26 +1000

    create basic layout

--- memory-game/src/App.tsx
+++ memory-game/src/App.tsx
@@ -1,10 +1,30 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [grid, setGrid] = useState([
+    [1, 3, 4, 6],
+    [3, 6, 7, 5],
+    [1, 7, 5, 4],
+    [9, 0, 0, 9],
+  ]);
   return (
     <div className="App">
-
+      <div className="container">
+        {grid.map((row, rowIndex) => {
+          return (
+            <div className="row" key={rowIndex}>
+              {row.map((col, colIndex) => {
+                return (
+                  <div className="card" key={colIndex}>
+                    {col}
+                  </div>
+                );
+              })}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
